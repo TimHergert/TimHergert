fc302815fd501b7bb4352d178cf3aa07ae100ae5
TimHergert <the43439@stud.hs-furtwangen.de>
2024-10-21 14:47:52 +0200

    RandomPoem_neu

--- A01_RandomPoem/randompoem.ts
+++ A01_RandomPoem/randompoem.ts
@@ -1,14 +1,22 @@
- console.log("Expecto Patronum");
-namespace RandomPoem{
-    let subjekte: string[] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
-    let prädikate: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
-    let objekte: string[] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
-console.log({subjekte, prädikate, objekte})
-for (let i = subjekte.length; i >= 1; i --) {
-    let Ergebniss: string = getVerse([],[],[])
-    console.log(Ergebniss, i)
-}
-function getVerse(_subjekte: string[],_prädikate: string [], _objkete: string[]): string {
-    return "alohomora"
-}
+console.log("Expecto Patronum");
+namespace RandomPoem {
+    let subjects: string[] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+    let predicate: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+    let objects: string[] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+    console.log({ subjects, predicate, objects })
+    for (let i = subjects.length; i >= 1; i--) {
+        let Ergebniss: string = getVerse(subjects, predicate, objects)
+        console.log(Ergebniss, i)
+    }
+    function getVerse(_subjects: string[], _predicate: string[], _objects: string[]): string {
+        let verse: string = "";
+       let subjectnumber: number = Math.floor(Math.random()*_subjects.length);
+       let subjectresult: number = Math.floor(Math.random()*_subjects.length);
+       let predicatenumber: number = Math.floor(Math.random()*_predicate.length);
+       let predicateresult: number = Math.floor(Math.random()*_predicate.length);
+       let objectnumber: number = Math.floor(Math.random()*_objects.length);
+       let objectresult: number = Math.floor(Math.random()*_objects.length);
+        return "alohomora"
+
+    }
 }
