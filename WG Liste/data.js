4b3f9cf6bddacf1f74eef05127c124bb01d3957e
TimHergert <the43439@stud.hs-furtwangen.de>
2024-11-29 15:32:14 +0100

    canvas

---
+++ WG Liste/data.js
@@ -0,0 +1,9 @@
+"use strict";
+var A03_Formular;
+(function (A03_Formular) {
+    /* export interface Data {
+        [value: string]: Column[];
+    } */
+    A03_Formular.data = [];
+})(A03_Formular || (A03_Formular = {}));
+//# sourceMappingURL=data.js.map
---
+++ WG Liste/data.ts
@@ -0,0 +1,20 @@
+namespace A03_Formular {
+
+    export interface Column {
+        Title: string;
+        For: string;
+        Date: string;
+        Time: string;
+        Comment: string;
+        Status: string;
+    }
+
+    /* export interface Data {
+        [value: string]: Column[];
+    } */
+
+    export let data: Column[] = [
+
+
+    ]
+}
